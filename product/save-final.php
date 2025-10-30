335b78eed0f2c0bb0e4f39835cb1aa20281ce145
Zitihayath Worawatthanachai <153045749+cs6636127@users.noreply.github.com>
2025-10-31 03:47:20 +0700

    Delete product directory

--- product/save-final.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-if (isset($_POST['finalprice'])) {
-    $_SESSION["finalPrice"] = floatval($_POST['finalprice']);
-}
-?>
--- product/save-promo.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-if (isset($_POST['promo'])) {
-    $_SESSION['id_promo'] = $_POST['promo'];
-}
-?>
